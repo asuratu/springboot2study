26ef27870181db4e90aa82a762c21d610b377089
AsuraTu <tugecn@gmail.com>
2023-07-24 16:23:58 +0800

    update

---
+++ springboot_01_01_quickstart/src/main/java/com/heima/Springboot0101QuickstartApplication.java
@@ -0,0 +1,16 @@
+package com.heima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author asura
+ */
+@SpringBootApplication
+public class Springboot0101QuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+    }
+
+}
