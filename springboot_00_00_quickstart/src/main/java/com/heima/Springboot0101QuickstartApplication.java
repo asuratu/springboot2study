c2a6bff48f4146346d3976f17cbf6b2487063c0a
AsuraTu <tugecn@gmail.com>
2023-07-26 10:11:17 +0800

    update

---
+++ springboot_00_00_quickstart/src/main/java/com/heima/Springboot0101QuickstartApplication.java
@@ -0,0 +1,23 @@
+package com.heima;
+
+import com.heima.controller.BookController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author asura
+ */
+@SpringBootApplication
+public class Springboot0101QuickstartApplication {
+
+    public static void main(String[] args) {
+        // 可配置应用上下文
+        ConfigurableApplicationContext ctx = SpringApplication.run (Springboot0101QuickstartApplication.class, args);
+        // 获取 bean 的四种方式
+        BookController bean = ctx.getBean(BookController.class);
+
+        System.out.println("===========>" + bean);
+    }
+
+}
---
+++ springboot_02_base_configuration/src/main/java/com/heima/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.heima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author asura
+ */
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @GetMapping
+    public String getById() {
+        System.out.println("springboot is running...3");
+        return "springboot is running...3";
+    }
+}
