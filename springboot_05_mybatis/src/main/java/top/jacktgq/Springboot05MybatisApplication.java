49412d1a77f7a05e942d9397001ab6cd68c214f4
AsuraTu <tugecn@gmail.com>
2023-07-27 10:42:12 +0800

    整合Mybatis

---
+++ springboot_05_mybatis/src/main/java/top/jacktgq/Springboot05MybatisApplication.java
@@ -0,0 +1,16 @@
+package top.jacktgq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author asura
+ */
+@SpringBootApplication
+public class Springboot05MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot05MybatisApplication.class, args);
+	}
+
+}
---
+++ springboot_05_mybatis/src/main/java/top/jacktgq/dao/BookDao.java
@@ -0,0 +1,15 @@
+package top.jacktgq.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import top.jacktgq.domain.Book;
+
+/**
+ * @author AsuraTu
+ */
+@Mapper // 表示这是一个mybatis的mapper类
+public interface BookDao {
+
+    @Select("select * from tbl_book where id = #{id}")
+    Book getBookById(Integer id);
+}
