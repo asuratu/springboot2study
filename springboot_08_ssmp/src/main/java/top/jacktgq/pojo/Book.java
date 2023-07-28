84e53ffbc1d093cb3dcf7bfa2b19debf6453512b
AsuraTu <tugecn@gmail.com>
2023-07-28 09:45:50 +0800

    ssmp

--- springboot_08_ssmp/src/main/java/top/jacktgq/pojo/Book.java
+++ springboot_08_ssmp/src/main/java/top/jacktgq/pojo/Book.java
@@ -1,7 +1,5 @@
 package top.jacktgq.pojo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +11,7 @@ import org.springframework.stereotype.Component;
 // @AllArgsConstructor
 // @NoArgsConstructor
 public class Book {
-    @TableId(value = "id", type = IdType.AUTO)
+    // @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
     private String name;
     private String type;
--- springboot_08_ssmp/src/test/java/top/jacktgq/mapper/BookMapperTest.java
+++ springboot_08_ssmp/src/test/java/top/jacktgq/mapper/BookMapperTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.jacktgq.pojo.Book;
 
+import java.util.List;
+
 /**
  * @author AsuraTu
  */
@@ -25,10 +27,36 @@ public class BookMapperTest {
         book.setDescription("Java是一门面向对象的编程语言");
 
         bookMapper.insert(book);
+
+        Book book1 = new Book();
+        book1.setName("神秘岛");
+        book1.setType("科幻");
+        book1.setDescription("《神秘岛》是法国科幻小说家儒勒·凡尔纳创作的长篇小说，是其三部曲的第三部，全书共3部62章。");
+
+        bookMapper.insert(book1);
     }
 
     @Test
-    void testGetById() {
+    void testGetBookById() {
         System.out.println(bookMapper.getBookById(1));
     }
+
+    @Test
+    void testSelectById() {
+        System.out.println(bookMapper.selectById(1));
+    }
+
+    @Test
+    void testDeleteById() {
+        System.out.println(bookMapper.deleteById(30)); // 返回值为影响的行数 1
+    }
+
+    @Test
+    void testSelectAll() {
+        List<Book> books = bookMapper.selectList(null);
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
 }
