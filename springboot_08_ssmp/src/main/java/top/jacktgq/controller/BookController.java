6a37ba753ea45ce32452557fab07bb8e1458e38f
AsuraTu <tugecn@gmail.com>
2023-07-29 17:18:53 +0800

    update

--- springboot_08_ssmp/src/main/java/top/jacktgq/controller/BookController.java
+++ springboot_08_ssmp/src/main/java/top/jacktgq/controller/BookController.java
@@ -1,9 +1,7 @@
 package top.jacktgq.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.jacktgq.pojo.Book;
 import top.jacktgq.service.IBookService;
 
@@ -27,5 +25,31 @@ public class BookController {
     @GetMapping
     public List<Book> index() {
         return bookService.list();
+    }
+
+    // 保存书籍
+    @PostMapping
+    public String save(@RequestBody Book book) {
+        bookService.save(book);
+        return "success";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@RequestBody Book book, @PathVariable("id") Integer id) {
+        System.out.println(">>>>>> id = " + id);
+        book.setId(id);
+        bookService.updateById(book);
+        return "success";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        bookService.removeById(id);
+        return "success";
+    }
+
+    @GetMapping("/{id}")
+    public Book get(@PathVariable("id") Long id) {
+        return bookService.getById(id);
     }
 }
